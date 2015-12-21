46e08aecdc2b9165c9007af04a2de5b2c0c95a4d
Sam Chiu <s951010sam@gmail.com>
2015-12-21 09:45:12 +0800

    add composite class

--- src/CompositeDemo.java
+++ src/CompositeDemo.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.ListIterator;
+
 
 public class CompositeDemo {
 
@@ -6,7 +9,43 @@ public class CompositeDemo {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		//test
 	}
 
+}
+
+
+//"Component"
+abstract class Component {
+	abstract void op();
+}
+
+//"Composite"
+class Composite extends Component {
+	ArrayList<Component> list;
+	
+	public Composite() {
+	   list = new ArrayList<Component>();
+	}
+	
+	void addComponent(Component c) {
+	   list.add(c);
+	}
+
+	// "operation()" in the "Composite"
+	void op() {
+		 ListIterator<Component> iterator = list.listIterator();
+		 while (iterator.hasNext()) {
+		   Component c = iterator.next();
+		   c.op();
+		 }
+	}  
+}
+
+//"Client"
+class Main {
+	void m1(Component[] cc) {
+	 for (Component c: cc) 
+	   c.op();
+	}
 }
